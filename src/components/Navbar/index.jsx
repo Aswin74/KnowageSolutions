ea89fb895e01f3599b037be3985ef8b2f7ffa7d1
Aswin <jayaprakash.jp713@gmail.com>
2024-06-02 11:10:11 +0530

    Responsive Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,19 +5,31 @@ import Button from "../Button";
 import logo from "../../assets/KS_icon.png";
 import MenuSvg from "../../assets/svg/MenuSvg";
 import { navigation } from "../../constants";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 function Navbar() {
   const currPath = useLocation();
 
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleToggle = () => {
+    if (isOpen) {
+      setIsOpen(false);
+      enablePageScroll();
+    } else {
+      setIsOpen(true);
+      disablePageScroll();
+    }
+  };
+
+  const handleNavClick = () => {
+    enablePageScroll();
+    setIsOpen(false);
+  };
+
   return (
     <>
-      <nav
-        className={`fixed top-0 left-0 right-0 z-50 border-b border-n-5 ${
-          !isOpen ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
-        } `}
-      >
+      <nav className="fixed top-0 left-0 right-0 z-50 bg-n-8 border-b border-n-5">
         <div className="flex items-center justify-between px-5 lg:px-7.5">
           <a href="#hero" className="block w-[12rem] lg:mr-8 ml-2">
             <img src={logo} className="h-20" alt="Knowage Solutions" />
@@ -27,21 +39,21 @@ function Navbar() {
           <div
             className={`${
               isOpen ? "flex" : "hidden"
-            } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:flex lg:static lg:mx-auto lg:bg-transparent`}
+            } fixed top-[5rem] left-0 right-0 bottom-0 ${
+              !isOpen ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+            } lg:flex lg:static lg:mx-auto lg:bg-transparent`}
           >
             <div className="relative z-2 flex flex-col lg:flex-row items-center justify-center m-auto">
               {navigation.map((item) => (
                 <a
                   key={item.id}
                   href={item.url}
-                  className={`block relative font-code uppercase text-n-1 transition-colors hover:text-color-1 ${
+                  className={`block relative font-code uppercase transition-colors hover:text-color-1 ${
                     item.onlyMobile ? "lg:hidden" : ""
                   } p-6 lg:-mr-0.25 lg:text-sm lg:font-semibold ${
-                    item.url === currPath.hash
-                      ? "z-2 lg:text-n-1"
-                      : "lg:text-n-1/50"
+                    item.url === currPath.hash ? "z-2 text-n-1" : "text-n-1/50"
                   } lg:leading-5 lg:hover:text-n-1`}
-                  onClick={() => setIsOpen(false)}
+                  onClick={handleNavClick}
                 >
                   {item.title}
                 </a>
@@ -54,7 +66,7 @@ function Navbar() {
               Register
             </Button>
 
-            <Button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+            <Button className="lg:hidden" onClick={handleToggle}>
               <MenuSvg openNavigation={isOpen} />
             </Button>
           </div>
