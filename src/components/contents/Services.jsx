50c90eefb10440afcc9bf6d17f60e1efc53e0a83
Aswin <jayaprakash.jp713@gmail.com>
2024-06-06 13:23:26 +0530

    S:Card component,Style

--- src/components/contents/Services.jsx
+++ src/components/contents/Services.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import Section from "../Section";
-import { BottomLine } from "../../assets/design/BackgroudDesigns";
 import Heading from "../Heading";
+import { smallSphere } from "../../assets";
+import { BottomLine } from "../../assets/design/BackgroudDesigns";
+
+const Card = ({ className, img, title, text }) => {
+  // Reusable Card Component for below
+  return (
+    <div
+      className={`${className} h-30 flex p-3 items-center backdrop-blur-[2px]
+			border-2 border-stroke-tint shadow-md shadow-cyan-300/50 rounded-3xl overflow-hidden`}
+    >
+      <img src={img} className="h-24 w-24 m-2 mr-7 md:mr-10" alt="icon" />
+      <div className="block">
+        <h5 className="h5 font-code font-semibold text-ks-primary">{title}</h5>
+        {text && <p className="body-2 max-w-72">{text}</p>}
+      </div>
+    </div>
+  );
+};
 
 const Services = () => {
   return (
@@ -9,30 +26,42 @@ const Services = () => {
       <div className="container relative z-2 mb-28">
         <Heading
           title="Our Free Services"
-          className="md:max-w-md mb-20 lg:max-w-2xl"
+          className="md:max-w-md mb-14 lg:max-w-2xl"
         />
 
         {/* Service Bento Grid */}
         <div className="grid grid-cols-1 gap-4 max-w-[50rem] mx-auto lg:grid-cols-3">
-          <div className="col-span-1 h-64 p-3 backdrop-blur border-2 border-stroke-tint rounded-3xl overflow-hidden">
-            college admission
+          <Card
+            img={smallSphere}
+            title="College Admission"
+            className="col-span-1 lg:h-64 lg:flex-col lg:justify-evenly lg:text-center"
+          />
+
+          <div className="grid col-span-2 gap-4">
+            <Card
+              img={smallSphere}
+              title="Course Selection"
+              className="lg:h-[7.5rem]"
+            />
+            <Card
+              img={smallSphere}
+              title="College Selection"
+              className="lg:h-[7.5rem]"
+            />
           </div>
 
-          <div className="col-span-2 gap-4">
-            <div className="h-32 mb-3 p-3 backdrop-blur border-2 border-stroke-tint rounded-3xl overflow-hidden">
-              course selection
-            </div>
-            <div className="h-28 p-3 backdrop-blur border-2 border-stroke-tint rounded-3xl overflow-hidden">
-              college selection
-            </div>
-          </div>
+          <Card
+            img={smallSphere}
+            title="Scholarship"
+            text="Ranging from ₹10,000 to ₹1,00,000 for all students who join through us."
+            className="col-span-2 lg:h-56"
+          />
 
-          <div className="col-span-2 h-32 p-3 backdrop-blur border-2 border-stroke-tint rounded-3xl overflow-hidden">
-            Scholarship
-          </div>
-          <div className="col-span-1 h-32 p-3 backdrop-blur border-2 border-stroke-tint rounded-3xl overflow-hidden">
-            Free of Cost
-          </div>
+          <Card
+            img={smallSphere}
+            title="Free College Visit"
+            className="col-span-1 lg:h-56 lg:flex-col lg:text-center"
+          />
         </div>
       </div>
       <BottomLine />
